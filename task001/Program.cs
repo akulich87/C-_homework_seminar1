8d763b0fc423e09f8c956657124d09b0d02a7983
akulich87 <amgrupp_project@mail.ru>
2023-03-12 21:01:53 +0300

    add task001

--- task001/Program.cs
+++ task001/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает 
+// два числа и выдает, какое число больше, а какое меньше.
+
+Console.WriteLine("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 > number2)
+{
+  Console.WriteLine("Первое число больше второго ");
+}
+
+if(number1 < number2)
+{
+  Console.WriteLine("Второе число больше первого ");
+}
+
+if(number1 == number2)
+{
+  Console.WriteLine("Числа равны ");
+}
