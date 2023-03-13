22b17ac2ab62178daaa2836a6fb172d72a6ac987
akulich87 <amgrupp_project@mail.ru>
2023-03-13 03:09:03 +0300

    add task 004

---
+++ task004/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N)
+// а на выходе показывает все четные числа от 1 до N.
+
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int first = 1;
+
+while (first<number)
+{
+  Console.WriteLine(first+1);
+  first+=2;
+}
